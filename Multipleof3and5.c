b625ed4f693026c50e88bd00c24f77d8be02295f
Leo Kool <kool@brightsight.com>
2015-05-20 14:10:26 +0200

    finished

--- Multipleof3and5.c
+++ Multipleof3and5.c
@@ -1,6 +1,6 @@
 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-// Find the sum of all the multiples of 3 or 5 below 1000.
+// Find the sum of all the multiples of 3 or 5 BELOW 1000.
 
 
 #include <stdio.h>
@@ -9,10 +9,12 @@
 int main(){
   
   int a = 0, b=0;
-  for(int i=0; i<10; i++){
   
-  printf("Help");
-  
-  }
+  for(int i=0; i<1000; i++){
+   
+    if (  i%5 == 0 || i%3 ==0  ){
+       b=b+i  ;
+       printf( "B =%i and I =  %i \n",b, i);}
+      }
   return 0; 
 }
