4649e4e184faedf5c15fb9e4c8a27e2edfa2cb0e
Leo Kool <kool@brightsight.com>
2015-05-20 09:45:23 +0200

    forloop

--- Multipleof3and5.c
+++ Multipleof3and5.c
@@ -1,7 +1,18 @@
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+
 #include <stdio.h>
 
 
 int main(){
+  
+  int a = 0, b=0;
+  for(int i=0; i<10; i++){
+  
   printf("Help");
+  
+  }
   return 0; 
 }
